d305af39577e4928c205f48d1586d370e9a0a33d
wangly@shishike.com <zxcvbnm123>
2017-06-27 16:45:09 +0800

    update

--- src/com/wly/simple_java/CheckArrayUtils.java
+++ src/com/wly/simple_java/CheckArrayUtils.java
@@ -33,8 +33,53 @@ public class CheckArrayUtils {
 		return false;
 	}
 	
+	/**
+	 * 错的！！！
+	 * @param arr
+	 * @param targetValue
+	 * @return
+	 */
 	public static boolean useArraysBinarySearch(String[] arr, String targetValue){
 		int a = Arrays.binarySearch(arr, targetValue);
 		return (a > 0) ? true : false;
+	}
+	
+	public static void main(String[] args) {
+		String[] arr = new String[]{"123","456","789","0AB","cde","fjh"};
+		
+		long startTime = System.currentTimeMillis();
+		for(int i = 0; i < 100000; i++){
+			useList(arr, "A");
+		}
+		long endTime = System.currentTimeMillis();
+		long duration = endTime - startTime;
+		System.out.println("use list: ---->" + duration);
+		
+		
+		long startTime2 = System.currentTimeMillis();
+		for(int i = 0; i < 100000; i++){
+			useSet(arr, "A");
+		}
+		long endTime2 = System.currentTimeMillis();
+		long duration2 =  endTime2 - startTime2;
+		System.out.println("use set: ---->" + duration2);
+		
+		
+		long startTime3 = System.currentTimeMillis();
+		for(int i = 0; i < 100000; i++){
+			useLoop(arr, "A");
+		}
+		long endTime3 = System.currentTimeMillis();
+		long duration3 = endTime3 - startTime3;
+		System.out.println("use loop: ---->" + duration3);
+		
+		
+		long startTime4 = System.currentTimeMillis();
+		for(int i = 0; i < 100000; i++){
+			useArraysBinarySearch(arr, "A");
+		}
+		long endTime4 = System.currentTimeMillis();
+		long duration4 = endTime4 - startTime4;
+		System.out.println("use binaryt: ---->" + duration4);
 	}
 }
